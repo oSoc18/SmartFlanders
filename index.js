5abd40da9bd843dd4cc64733a0ae5fc1ef58373b
ThibaultLesuisse <lesuisse.thibault@gmail.com>
2018-07-05 13:09:04 +0200

    feat(): poc

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
+const N3 = require('n3');
+const csv = require('node-csv').createParser();
+const { DataFactory } = N3;
+const { namedNode, literal, defaultGraph, quad } = DataFactory;
+const store = N3.Store();
 
+/**
+ * Parsing the csv file and filling the data object
+ * @param {string} - Path of CSV file
+ * 
+ */
+csv.mapFile('./ToevlaExport_20180531.csv', function(err, data) {
+    
+  console.log(data[0].AccommodatieNaam);
+  store.addQuad(
+    namedNode('http://example.org/public-buildings#' + data[0].AccommodatieNaam),
+    namedNode('http://dbpedia.org/ontology/elevatorCount'),
+    literal(0))
+});
 /**
  * @param {string} - Route path
  * 
  */
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    
+    res.send(store.getQuads());
+})
 
 /**
  * 
  * Server listener
  * @param {number} - Port number
- * 
  */
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
