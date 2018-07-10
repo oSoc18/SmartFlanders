817e02649401276a843531cdd2465991bd80c7b8
Lieselot Geirnaert <lieselot.geirnaert@studetn.odisee.be>
2018-07-10 09:14:22 +0200

    Fixed #40: input form Added inputform with styling

---
+++ frontend/script.js
@@ -0,0 +1,22 @@
+var checkBuildingAddress = function() {
+  var postcode = document.getElementById("postcode").value;
+  var street = document.getElementById("street").value;
+  var number = document.getElementById("number").value;
+
+  // check values
+
+  // get building
+}
+
+var checkBuildingUri = function() {
+  var uri = document.getElementById("uri").value;
+
+  // check values
+
+  // get building
+  document.getElementById("building").innerHTML = uri;
+}
+
+var addInfo = function() {
+
+}
