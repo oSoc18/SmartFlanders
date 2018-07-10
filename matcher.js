cd5f1d81d3c5989168d95adf0d88537df930713a
ThibaultLesuisse <lesuisse.thibault@gmail.com>
2018-07-10 10:46:33 +0200

    chore(): removed merge conflict

--- matcher.js
+++ matcher.js
@@ -1,5 +1,4 @@
 const csv = require("fast-csv");
-<<<<<<< HEAD
 let data = [];
 
 module.exports = function (params) {
@@ -39,48 +38,3 @@ function matchingAddress(row, params) {
 		}
 	}
 }
-=======
-let data = []; 
-
-module.exports = function(params){
-    csv
-    .fromPath("./toevla.csv", {headers: true})
-	.on("data", function(d){
-		matchingAddress(d, params);
-	})
-	.on("end", function(){
-		//console.log(array);
-		console.log("done");
-	})
-	.on("error", function(error){
-		console.log(error);
-	});
-}
-
-function matchingAddress(row, params) {
-    console.log(row);
-	console.log(params);
-
-	if(params &&row &&row.AccomodatieStraat.toLowerCase() == params.straat.toLowerCase()) {
-		switch(entranceAccesible) {
-			case "PLUS":
-			console.log("It's a match! +");
-			break;
-
-			case "PLMN":
-			console.log("It's a match! +/-");
-			break;
-
-			case "MIN":
-			console.log("It's a match! -");
-			break;
-
-			default:
-			console.log("It's a match! UNKNOWN");
-			break;
-		}
-		
-	}
-}
-
->>>>>>> chore(): :bug:
