e961477fd37b06baeac3564f1309808d254ec494
ThibaultLesuisse <lesuisse.thibault@gmail.com>
2018-07-11 09:27:26 +0200

    merge(test.js): removed transformer object identifier

--- test/test.js
+++ test/test.js
@@ -25,7 +25,7 @@ describe("Transformer", function() {
 	it("Transforming data with ToeVla data available", function() {
 		//?gemeente=roeselare&postcode=9300&straat=grote%20markt&huisnummer=1
 		var params = { gemeente:"roeselare", postcode: 8800, straat: "botermarkt", huisnummer: 2 };
-                transformer.transformer(params);
+                transformer(params);
 	});
 	/*it("Transforming wrong data", function() {
                 //?gemeente=blablba&postcode=0157&straat=earth&huisnummer=-1
