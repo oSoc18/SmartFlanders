7c6e66def2057a652317e62affebec2decd284bb
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-07-16 13:56:58 +0200

    Move deployment to BASH script

---
+++ .circleci/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Go to master branch version
+cd ~/build/SmartFlanders
+git reset --hard
+rm package-lock.json
+git pull
+git checkout master
+
+# Install back end
+npm install
+forever stopall
+forever start index.js
+
+# Install frond end
+cd /var/www/smartflanders.ilabt.imec.be
+rm *.html *.css *.js *.scss
+cp ~/build/SmartFlanders/frontend /var/www/smartflanders.ilabt.imec.be
+
