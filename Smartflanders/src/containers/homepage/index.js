bcdfa7f29d86a9a1a1ae0c99e94698ea1b745385
Thibault Lesuisse <lesuisse.thibault@gmail.com>
2018-07-24 17:02:57 +0200

    chore(): Lieslot wanted a push

--- Smartflanders/src/containers/homepage/index.js
+++ Smartflanders/src/containers/homepage/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-
+import { Search } from '../search'
 export class Homepage extends Component {
     render() {
       return (
@@ -8,6 +8,7 @@ export class Homepage extends Component {
           <div className="intro">
             <h3>Structureer en deel informatie over publieke gebouwen en diensten voor uw lokaal bestuur</h3>
             <a className="button"><Link to="search-building">Zoek gebouw</Link></a>
+            <Search />
           </div>
         </div>
       );
