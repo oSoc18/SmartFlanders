7bcd906ddced7f02a86224ef53668dbb802f3aa5
Lieselot Geirnaert <lieselot.geirnaert@studetn.odisee.be>
2018-07-25 11:20:41 +0200

    buildinginfo styles

--- Smartflanders/src/components/snippet/index.js
+++ Smartflanders/src/components/snippet/index.js
@@ -4,9 +4,11 @@ export class Snippet extends Component {
     render() {
         return (
             <div className="snippet">
-	       <pre>
-                 {JSON.stringify(this.props.jsonld, null, 4)}
-               </pre>
+                <h3>JSON-LD snippet</h3>
+                <p>Kopieer deze snippet en plaats deze in de html van de webpagina van het gebouw</p>
+                <pre>
+                    {JSON.stringify(this.props.jsonld, null, 4)}
+                </pre>
             </div>
         )
     }
